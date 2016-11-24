057f478129645284ec1f15e1036c59ebd50ed48e
QuangSang <sang@bliss-interactive.com>
2016-11-24 11:47:01 +0700

    product_details

---
+++ modules/product_details.php
@@ -0,0 +1,74 @@
+<section class="page details" id="details">
+    <div class="inner">
+        <div class="container">
+            <div class="row bread">
+                <div class="col-md-6">
+                    <h2 class="page-title"><strong>Mid</strong> Centure</h2>
+                </div>
+                <div class="col-md-6">
+                    <ol class="breadcrumb pull-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Living Room</a></li>
+                        <li class="breadcrumb-item">Mid Centure</li>
+                    </ol>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+
+                </div>
+                <div class="col-md-5">
+                    <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis dolor repellendus quis nostrum.
+                    </p>
+                    <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. </p>
+                    <h5 class="text-uppercase status">available</h5>
+                    <ul class="list-unstyled list-inline">
+                        <li><a href=""><img src="images/icon-fb.png" alt=""></a></li>
+                        <li><a href=""><img src="images/icon-twitter.png" alt=""></a></li>
+                        <li><a href=""><img src="images/icon-pintester.png" alt=""></a></li>
+                        <li><a href=""><img src="images/icon-google.png" alt=""></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <div class="price">
+                        <span class="price-detail price-retail">$ 140.00</span>
+                        <span class="price-detail price-sale">$ 130.00</span>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <label for="pwd" class="label-height">Quantity</label>
+                            </div>
+                            <div class="col-md-9">
+                                <input type="number" value="1" class="form-control" id="pwd">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <label for="pwd" class="label-height">Color</label>
+                            </div>
+                            <div class="col-md-9">
+                                <select name="" id="" class="form-control">
+                                    <option value="">Gray</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <label for="pwd" class="label-height">Size</label>
+                            </div>
+                            <div class="col-md-9">
+                                <select name="" id="" class="form-control">
+                                    <option value="" selected hidden disabled>M</option>
+                                </select>
+                            </div>
+                        </div>
+                        <p class="pull-right">
+                            SUB-TOTAL: $ 0.00
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
