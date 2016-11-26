20f3b4cf1912de3448d544751e0c33c9b53e16ce
QuangSang <sang@bliss-interactive.com>
2016-11-27 00:02:59 +0700

    acount page

---
+++ modules/accounts.php
@@ -0,0 +1,25 @@
+<section class="page acoounts" id="acoounts">
+    <div class="inner">
+        <div class="container">
+            <div class="row bread">
+                <div class="col-md-6">
+                    <span class="page-title"><strong>My</strong> Account</span>
+                </div>
+                <div class="col-md-6">
+                    <ol class="breadcrumb pull-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item">My Account</li>
+                    </ol>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5">
+                    <img src="images/bg-account.png" class="img-responsive" alt="">
+                </div>
+                <div class="col-md-7">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
