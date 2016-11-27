327a957cb8b488eda59e1da075f8e0eb50feb8a7
QuangSang <sang@bliss-interactive.com>
2016-11-27 11:08:16 +0700

    acount page done

--- modules/accounts.php
+++ modules/accounts.php
@@ -13,11 +13,65 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-5">
+                <div class="col-md-6">
                     <img src="images/bg-account.png" class="img-responsive" alt="">
                 </div>
-                <div class="col-md-7">
-
+                <div class="col-md-6">
+                    <h5 class="text-uppercase text-title">sign in</h5>
+                    <div class="row">
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <input type="email" placeholder="Email" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="password" placeholder="Password" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="" class="link underline">Forgotten account?</a>
+                        </div>
+                        <div class="col-md-12">
+                            <a href="" class="btn-contact text-uppercase">sign in</a>
+                        </div>
+                    </div>
+                    <h5 class="text-uppercase text-title register">register</h5>
+                    <p class="margin-bottom-25">Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.</p>
+                    <div class="row">
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <input type="text" placeholder="First Name" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="text" placeholder="Sur-Name" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <input type="email" placeholder="Email" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="text" placeholder="Phone" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <input type="password" placeholder="Password" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="password" placeholder="Re-enter Password" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group margin-bottom-25">
+                            <div class="col-md-12">
+                                <textarea class="form-control" placeholder="Address"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <a href="" class="btn-contact text-uppercase">register</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
